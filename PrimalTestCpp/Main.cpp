56f820c0b0adfc1737bddcd25071ee7cf2ff451b
Attila Szücs <ati.szucs@hotmail.com>
2024-09-10 11:19:24 +0200

    Leak detection

--- PrimalTestCpp/Main.cpp
+++ PrimalTestCpp/Main.cpp
@@ -1,11 +1,16 @@
+#define _CRTDBG_MAP_ALLOC
 #include <iostream>
 #include <fstream>
 #include <sstream>
 #include "Level.h"
 #include "ConsoleTools.h"
+#include <stdlib.h>
+#include <crtdbg.h>
 
 int main()
 {
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+
 	std::ifstream file("lvl.txt");
 	if (!file.is_open())
 		return 1;
@@ -36,4 +41,5 @@ int main()
 	default:
 		throw 0;
 	}
+	file.close();
 }
